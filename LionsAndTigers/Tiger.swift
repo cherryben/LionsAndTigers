7a6b24d226c502731dc494dcf8ae1a2d73aca0aa
Ben Coombes <Ben@Bens-iMac.local>
2014-09-29 15:48:00 +0100

    first commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,19 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Ben Coombes on 29/09/2014.
+//  Copyright (c) 2014 Ben Coombes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+}
